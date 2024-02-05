acbf52683e2647fb19db3349a13bed82c403691a
wacoo <wabaham9@gmail.com>
2024-02-05 10:27:00 +0300

    React setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello!</Typography>
+        </div>
+    )
+}
+
+export default App;
